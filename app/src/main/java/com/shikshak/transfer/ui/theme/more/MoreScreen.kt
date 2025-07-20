689c2d96d8143f940b1b75dfaff5aba4ea37fa88
Sohrab Alam <34127382+md-sohrab-alam@users.noreply.github.com>
2025-07-20 17:38:01 +0300

    Added emy email address

--- app/src/main/java/com/shikshak/transfer/ui/theme/more/MoreScreen.kt
+++ app/src/main/java/com/shikshak/transfer/ui/theme/more/MoreScreen.kt
@@ -1,5 +1,9 @@
 package com.shikshak.transfer.ui.theme.more
 
+import android.content.Context
+import android.content.Intent
+import android.net.Uri
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.verticalScroll
@@ -9,6 +13,7 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextDecoration
 import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
 import com.google.firebase.auth.FirebaseAuth
